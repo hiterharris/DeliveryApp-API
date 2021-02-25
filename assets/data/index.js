58053fafbe6b0e943c0c925aba49f0f34f6aa46d
hiterharris <hiterharris@comcast.net>
2021-02-25 09:51:45 -0500

    update db naming

---
+++ assets/data/index.js
@@ -0,0 +1,4 @@
+const { usersData } = require('./users');
+const { productsData, beerData, wineData, tobaccoData, vapeData } = require('./products');
+
+module.exports = { usersData, productsData, beerData, wineData, tobaccoData, vapeData }
---
+++ assets/data/users.js
@@ -0,0 +1,18 @@
+const usersData = [
+    {
+        id: 1,
+        name: 'Hiter',
+        email: 'hiter@gmail.com',
+        username: 'hiter',
+        password: 'pass',
+    },
+    {
+        id: 2,
+        name: 'Noor',
+        email: 'noor@gmail.com',
+        username: 'noor',
+        password: 'pass02',
+    }
+];
+
+module.exports = { usersData }
